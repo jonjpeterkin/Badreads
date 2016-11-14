5f0680e8cfd22a5daa91bee1f4ed90d5826c523a
James Segarra III <JamesSegarra@gmail.com>
2016-11-14 16:32:11 -0500

    working

--- scripts/controllers/books_controller.js
+++ scripts/controllers/books_controller.js
@@ -4,7 +4,7 @@ function populateBooks() {
     $('#book_row').append(`
       <div class="col-sm-4">
         <div class="w3-card-8">
-          <img class="w3-center" src="${book.coverImg}">
+          <img class="center" src="${book.coverImg}">
           <h4 class="w3-center">${book.title}</h4>
           <hr>
           <h4 class="w3-center">Average Rating:  ${book.averageRating}</h4>
